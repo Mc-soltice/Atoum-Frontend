79668207f9f2dc6560853352fd1781351a2d9a5b
Mc-soltice <166833249+Mc-soltice@users.noreply.github.com>
2026-03-04 10:05:52 +0100

    Update axios.ts
    verif

--- lib/axios.ts
+++ lib/axios.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL, 
   console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+
   withCredentials: true, // IMPORTANT pour Sanctum
   headers: {
     Accept: "application/json",
